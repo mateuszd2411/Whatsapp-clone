aebea0d9bc75538e11497547283c63afbb7f3990
Mateusz <mateuszd2411@gmail.com>
2020-01-28 17:51:23 +0100

    8.0

--- app/src/main/java/com/example/whatsapp/RegisterActivity.java
+++ app/src/main/java/com/example/whatsapp/RegisterActivity.java
@@ -2,13 +2,53 @@ package com.example.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText UserEmail, UserPassword;
+    private TextView AlreadyHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        InitializeFields();
+
+        AlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SendUserToLoginActivity();
+
+            }
+        });
+
+    }
+
+    private void InitializeFields() {
+
+        CreateAccountButton = (Button) findViewById(R.id.register_button);
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        AlreadyHaveAccount = (TextView) findViewById(R.id.already_have_account_link);
+
+
+    }
+
+    private void SendUserToLoginActivity() {
+
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+
     }
 }
