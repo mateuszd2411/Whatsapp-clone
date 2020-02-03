668eeecfd0f1471718239e51896fb83ac8876a26
Mateusz <mateuszd2411@gmail.com>
2020-02-03 11:18:25 +0100

    51.0

--- app/src/main/java/com/example/whatsapp/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/ChatActivity.java
@@ -144,6 +144,8 @@ public class ChatActivity extends AppCompatActivity {
 
                         messageAdapter.notifyDataSetChanged();
 
+                        userMessagesList.smoothScrollToPosition(userMessagesList.getAdapter().getItemCount());
+
                     }
 
                     @Override
